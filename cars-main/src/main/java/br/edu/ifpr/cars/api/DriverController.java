0dcaec5656227015e3388801eb2410a1a44bab48
Lais Mayer <20233007175@estudantes.ifpr.edu.br>
2025-10-20 08:14:58 -0300

    metodos

--- cars-main/src/main/java/br/edu/ifpr/cars/api/DriverController.java
+++ cars-main/src/main/java/br/edu/ifpr/cars/api/DriverController.java
@@ -3,12 +3,17 @@ package br.edu.ifpr.cars.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.edu.ifpr.cars.domain.Driver;
 import br.edu.ifpr.cars.domain.DriverRepository;
@@ -25,11 +30,27 @@ public class DriverController {
     public List<Driver> listDrivers() {
         return driverRepository.findAll();
     }
-
+    // definir uma exception personalizada
     @GetMapping("/drivers/{id}")
     public Driver findDriver(@PathVariable("id") Long id) {
-        return driverRepository.findById(id).get();
+        return driverRepository.findById(id).orElseThrow(
+            ()-> new ResponseStatusException(HttpStatus.NOT_FOUND)
+        );
+
+    }
+    @PostMapping("/drivers")
+    public Driver createDriver(@RequestBody Driver driver){
+        return driverRepository.save(driver);
+
     }
 
+    //uptade
+    @PutMapping("/drivers{id}")
+    public Driver fullUpdateDriver(@PathVariable("id") Long id, @RequestBody Driver driver){
+        Driver founDriver = findDriver(id);
+        foundDriver.setName(driver.getName());
+        foundDriver.setBirthDate(driver.getBirthDate());
+        return driverRepository.save(foundDriver);
+    }
 
 }
