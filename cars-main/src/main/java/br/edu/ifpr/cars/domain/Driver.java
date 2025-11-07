9de17f97c6bf28c64057a2177eb1fd21131a5c70
Lais Mayer <20233007175@estudantes.ifpr.edu.br>
2025-11-07 15:36:28 -0300

    Implementação atividade driver e passenger

--- cars-main/src/main/java/br/edu/ifpr/cars/domain/Driver.java
+++ cars-main/src/main/java/br/edu/ifpr/cars/domain/Driver.java
@@ -2,12 +2,17 @@ package br.edu.ifpr.cars.domain;
 
 import java.time.LocalDate;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Entity
@@ -17,12 +22,20 @@ public class Driver {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
     @NotBlank(message = "é necessário digitar um nome" )
+    @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 50 caracteres") 
+    @Pattern(regexp = "^[\\S]+$", message = "O nome não pode conter espaços em branco")
     @Valid
-    String name;
+    String name; 
+
     @NotBlank(message = "é necessário digitar um cpf" )
+    @CPF(message = "O CPF é inválido ou não foi preenchido corretamente.") 
     String cpf;
+
     @NotBlank(message = "é necessário digitar um email" )
+    @Email(message = "O email deve estar no formato válido") 
+    @Pattern(regexp = "^\\S+$", message = "O email não pode conter espaços em branco")
     String email;
+    
     LocalDate birthDate;
 
 }
--- cars-main/src/main/java/br/edu/ifpr/cars/domain/Passenger.java
+++ cars-main/src/main/java/br/edu/ifpr/cars/domain/Passenger.java
@@ -1,9 +1,16 @@
 package br.edu.ifpr.cars.domain;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,6 +19,20 @@ public class Passenger {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "é necessário digitar um nome" )
+    @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 50 caracteres") 
+    @Pattern(regexp = "^[\\S]+$", message = "O nome não pode conter espaços em branco")
+    @Valid
     private String name;
+
+    
+    @NotBlank(message = "é necessário digitar um cpf" )
+    @CPF(message = "O CPF é inválido ou não foi preenchido corretamente.") 
     private String email;
+
+    @NotBlank(message = "é necessário digitar um email" )
+    @Email(message = "O email deve estar no formato válido") 
+    @Pattern(regexp = "^\\S+$", message = "O email não pode conter espaços em branco")
+    private String cpf;
 }
